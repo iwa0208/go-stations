c7bfe552463ab21b9b62978f70a82e006d58dd85
iwa0208 <iwacchi28@gmail.com>
2022-11-15 23:27:07 +0900

    station 5 clear

--- handler/router/router.go
+++ handler/router/router.go
@@ -3,10 +3,15 @@ package router
 import (
 	"database/sql"
 	"net/http"
+	"github.com/TechBowl-japan/go-stations/handler"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz/", handler.NewHealthzHandler().ServeHTTP)
+	// 下記の2パターンで実行可能
+	// mux.HandleFunc("/healthz/", handler.NewHealthzHandler().ServeHTTP)
+	// mux.HandleFunc("/healthz/", new(handler.HealthzHandler).ServeHTTP)
 	return mux
 }
